6700b27d80d909d0fa9914b17a52e75665b0947b
LouisWanjao <louis.wanjao@strathmore.edu>
2024-05-15 22:53:24 +0300

    add node function

--- app/consistent_hashing.py
+++ app/consistent_hashing.py
@@ -27,3 +27,17 @@ class ConsistentHashing:
         node_hash = int(hashlib.md5(node.encode("utf-8")).hexdigest(), 16)
         # Calculate the position of the virtual node on the hash ring
         return (node_hash**2 + replica**2 + 2 * replica + 25) % self.num_slots
+    
+    def add_node(self, node):
+        """
+        Add a physical node and its virtual replicas to the hash ring.
+        
+        Parameters:
+        node (str): The identifier of the physical node.
+        """
+        # Add virtual nodes for the given physical node
+        for i in range(9):  # Using K = 9 for 9 virtual replicas
+            hashed_key = self._hash_virtual(node, i)
+            self.nodes.append((hashed_key, node))  # Append (hashed_key, node) tuple
+        # Sort the list of nodes to maintain the order on the hash ring
+        self.nodes.sort()
